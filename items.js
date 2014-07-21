7c27c5927d2cc1e15d2910b68d5812dc32b71151
Matthew Chung <matthewchung74@gmail.com>
2014-07-20 19:01:23 -0700

    first run at adding mongoose changed index of items from number to mongodb _id

---
+++ items.js
@@ -0,0 +1,52 @@
+var mongoose = require('mongoose');
+
+// defines our schema
+var itemSchema = mongoose.Schema({
+	item: String,
+	index: Number
+})
+
+// creates our mongoose model
+var Item = mongoose.model('Item', itemSchema);
+
+exports.create = function(req, res){
+	if (!req.param('item')) {
+		res.send('Item not valid');
+		res.statusCode = 400;
+	} else {
+		var item = new Item({ item: req.param('item')});
+		console.log(item);
+		item.save(function (err, item) {
+			if (err) return console.error(err);
+			res.send("adding " + item);
+		});
+	}
+};
+
+exports.retrieveAll = function(req, res){
+	Item.find(function (err, items) {
+		if (err) return console.error(err);
+		res.send(items);
+	})
+};
+
+exports.retrieveOne = function(req, res) {
+	Item.findOne({'_id':mongoose.Types.ObjectId(req.param('mongoId'))}, function (err, item) {
+		if (err) return console.error(err);
+		res.send(item);
+	});
+}
+
+exports.update = function(req, res) {
+	Item.findByIdAndUpdate(mongoose.Types.ObjectId(req.param('mongoId')), {'item':req.param('item')}, function (err, result) {
+		if (err) return console.error(err);
+		res.send(result);
+	})
+}
+
+exports.delete = function(req, res) {
+	Item.remove({'_id':mongoose.Types.ObjectId(req.param('mongoId'))}, function (err, result) {
+		if (err) return console.error(err);
+		res.send({});
+	})
+}
--- server.js
+++ server.js
@@ -1,80 +1,44 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-
-var items = [];
+var mongoose = require('mongoose');
+var myitems = require('./items');
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use( bodyParser.urlencoded() ); // to support URL-encoded bodies
-
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
 
 // gets all items, ie http GET localhost:5000
-app.get('/', function(req, res){
-	items.forEach(function (item, i) {
-		res.write(i + '. ' + item + '\n');
-	});
-	res.end();
-});
+app.get('/', myitems.retrieveAll);
 
-// gets a single item, ie the second item http GET localhost:5000/1
-app.get('/:index', function(req, res){
-	if (isNaN(req.params.index)) {
-		res.send('Item id not valid');
-		res.statusCode = 400;
-	}
-	else if (items[req.params.index]) {
-		res.send("getting "+req.params.index);
-	}
-	else {
-res.send("item not found");
-}
-});
+// gets a single item, ie the second item http GET localhost:5000/id
+app.get('/:mongoId', myitems.retrieveOne);
 
 // adds and item, ie http --form POST localhost:5000 item='oranges'
-app.post('/', function(req, res){
-	if (!req.param('item')) {
-		res.send('Item not valid');
-		res.statusCode = 400;
-	} else {
-		items.push(req.param('item'));
-		res.send("adding " + req.param('item'));
-	}
-});
+app.post('/', myitems.create);
 
-// updates an item, ie http --form PUT localhost:5000/1 item='chocolate'
-app.put('/:index', function(req, res){
-	if (isNaN(req.params.index)) {
-		res.send('Item id not valid');
-		res.statusCode = 400;
-	}
-	else if (items[req.params.index] && req.param('item')) {
-		items[req.params.index] = req.param('item');
-		res.send("putting "+req.params.index);
-	}
-	else {
-		res.send("item not found");
-	}
-});
+// updates an item, ie http --form PUT localhost:5000/id item='chocolate'
+app.put('/:mongoId', myitems.update);
 
-// deletes an item, ie http DELETE localhost:5000/1
-app.delete('/:index', function(req, res){
-	if (isNaN(req.params.index)) {
-		res.send('Item id not valid');
-		res.statusCode = 400;
-	}
-	else if (items[req.params.index]) {
-		items.splice(req.params.index, 1);
-		res.end('Item deleted successfully');
-	}
-	else {
-		res.send("item not found");
-	}
-});
+// deletes an item, ie http DELETE localhost:5000/id
+app.delete('/:mongoId', myitems.delete);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function () {
 	console.log('listening on '+port);
 });
+
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+	// yay!
+});
+
+// Error handler
+mongoose.connection.on('error', function (err) {
+	console.log(err)
+})
+
