71a0581188d8af59674a02df58de992690badf3b
Matthew Chung <matthewchung74@gmail.com>
2014-07-21 19:03:46 -0700

    removed mongo references

--- items.js
+++ items.js
@@ -1,54 +1,59 @@
-var mongoose = require('mongoose');
-
-// defines our schema
-var itemSchema = mongoose.Schema({
-	item: String,
-	index: Number // may refactor out
-})
-
-// creates our mongoose model
-var Item = mongoose.model('Item', itemSchema);
+var items = [];
 
 exports.create = function(req, res){
 	if (!req.param('item')) {
 		res.send('Item not valid');
 		res.statusCode = 400;
 	} else {
-		var item = new Item({ item: req.param('item')});
-		console.log(item);
-		item.save(function (err, item) {
-			if (err) return console.error(err);
-			res.send("adding " + item);
-		});
+		items.push(req.param('item'));
+		res.send("adding " + req.param('item'));
 	}
 };
 
 exports.retrieveAll = function(req, res){
-	Item.find(function (err, items) {
-		if (err) return console.error(err);
-		res.send(items);
-	})
+	items.forEach(function (item, i) {
+		res.write(i + '. ' + item + '\n');
+	});
+	res.end();
 };
 
 exports.retrieveOne = function(req, res) {
-	// TODO: add some err handling
-	Item.findOne({'_id':mongoose.Types.ObjectId(req.param('mongoId'))}, function (err, item) {
-		if (err) return console.error(err);
-		res.send(item);
-	});
+	if (isNaN(req.params.id)) {
+		res.send('Item id not valid');
+		res.statusCode = 400;
+	}
+	else if (items[req.params.id]) {
+		res.send("getting "+req.params.id);
+	}
+	else {
+		res.send("item not found");
+	}
 }
 
 exports.update = function(req, res) {
-	// TODO: add some err handling
-	Item.findByIdAndUpdate(mongoose.Types.ObjectId(req.param('mongoId')), {'item':req.param('item')}, function (err, result) {
-		if (err) return console.error(err);
-		res.send(result);
-	})
+	if (isNaN(req.params.id)) {
+		res.send('Item id not valid');
+		res.statusCode = 400;
+	}
+	else if (items[req.params.id] && req.param('item')) {
+		items[req.params.id] = req.param('item');
+		res.send("putting "+req.params.id);
+	}
+	else {
+		res.send("item not found");
+	}
 }
 
 exports.delete = function(req, res) {
-	Item.remove({'_id':mongoose.Types.ObjectId(req.param('mongoId'))}, function (err, result) {
-		if (err) return console.error(err);
-		res.send({});
-	})
+	if (isNaN(req.params.id)) {
+		res.send('Item id not valid');
+		res.statusCode = 400;
+	}
+	else if (items[req.params.id]) {
+		items.splice(req.params.id, 1);
+		res.end('Item deleted successfully');
+	}
+	else {
+		res.send("item not found");
+	}
 }
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-var mongoose = require('mongoose');
 var myitems = require('./items');
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
@@ -13,32 +12,19 @@ app.use(bodyParser.urlencoded({
 app.get('/', myitems.retrieveAll);
 
 // gets a single item, ie the second item http GET localhost:5000/id
-app.get('/:mongoId', myitems.retrieveOne);
+app.get('/:id', myitems.retrieveOne);
 
 // adds and item, ie http --form POST localhost:5000 item='oranges'
 app.post('/', myitems.create);
 
 // updates an item, ie http --form PUT localhost:5000/id item='chocolate'
-app.put('/:mongoId', myitems.update);
+app.put('/:id', myitems.update);
 
 // deletes an item, ie http DELETE localhost:5000/id
-app.delete('/:mongoId', myitems.delete);
+app.delete('/:id', myitems.delete);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function () {
 	console.log('listening on '+port);
 });
-
-mongoose.connect('mongodb://localhost/test');
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function callback () {
-	// yay!
-});
-
-// Error handler
-mongoose.connection.on('error', function (err) {
-	console.log(err)
-})
 
